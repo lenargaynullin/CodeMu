b20ce4163905ce2b6fba1be99396033d66a57311
lenargaynullin <lenar.gaynullin88@gmail.com>
2026-01-30 09:23:40 +0300

    Добавлен класс Second Задача №2 Дана строка. Выведите в консоль длину этой строки.

---
+++ src/Second.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+// №2 Дана строка. Выведите в консоль длину этой строки.
+public class Second {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Your First Name: ");
+        String string = scanner.nextLine();
+        System.out.println(string.length());
+    }
+
+}
