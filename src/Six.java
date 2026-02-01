1c2663b047ac0e8a84f9da26650ee86b949b7ad8
lenargaynullin <lenar.gaynullin88@gmail.com>
2026-02-01 07:24:12 +0300

    Добавлен класс Six Дано целое число. Выведите в консоль первую цифру этого числа.

---
+++ src/Six.java
@@ -0,0 +1,7 @@
+// Дано целое число. Выведите в консоль первую цифру этого числа.
+public class Six {
+    public static void main(String[] args) {
+        int number = 123456;
+        System.out.println(String.valueOf(number).charAt(0)); // Первая цифра
+    }
+}
