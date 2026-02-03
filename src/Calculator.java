aa168e53595e0a47266b0139a2b62d17595a278b
lenargaynullin <lenar.gaynullin88@gmail.com>
2026-02-03 15:40:17 +0300

    Добавлен класс Calculator Задача на ООП

---
+++ src/Calculator.java
@@ -0,0 +1,6 @@
+public class Calculator {
+
+    int sum (int x, int y) {
+        return x + y;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Person person = new Person();
-        person.setName("Lenar");
-        person.setAge(18);
-        System.out.println(person);
-        System.out.println(person.getAge());
-        System.out.println(person.getName());
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.sum(2,3));
     }
 }
